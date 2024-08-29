123355b59457beed6f31088563af0fad34604d4c
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-08-29 23:15:55 +0500

    implement slider on product list

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 const menuBtn = document.querySelector(".menu-icon");
 const navLinkContainer = document.querySelector(".nav-link-container");
 const closeBtn = document.querySelector(".close-menu");
-
+const slider = document.querySelector(".product-card-container");
+let isDown = false;
+let startX;
+let scrollLeft;
 
 menuBtn.addEventListener("click", () => {
   navLinkContainer.classList.remove("nav-link-hide");
 });
-
 
 closeBtn.addEventListener("click", () => {
   navLinkContainer.classList.add("nav-link-hide");
 });
 
 
-
 //  slider
-// const slider = document.querySelector('.product-card-container');
 
-// slider.addEventListener('scroll', () => {
-//     let scrollLeft = slider.scrollLeft;
-//     let cardWidth = slider.querySelector('.card').offsetWidth;
-//     let scrollTo = Math.round(scrollLeft / cardWidth) * cardWidth;
-    
-//     slider.scrollTo({
-//         left: scrollTo,
-//         behavior: 'smooth'
-//     });
-// });
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.style.cursor = "grabbing";
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = x - startX;
+  slider.scrollLeft = scrollLeft - walk;
+});
